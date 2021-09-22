383f9aba868e786d1a24cbf64df2a8c7d75609b5
lklet123 <42313439+lklet123@users.noreply.github.com>
2021-09-23 02:09:00 +0800

    Update proxyChecker.py

--- proxyChecker.py
+++ proxyChecker.py
@@ -1,12 +1,18 @@
 import urllib.request
 import threading
 import random
-import sys
-import os
 import argparse
-from time import time
+from time import time, sleep
 
-useragents=('Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')
+user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.94 Chrome/37.0.2062.94 Safari/537.36"
+
+def random_user_agent(file='user_agents.txt'):
+	with open(file, "r") as f:
+		lines = f.readlines()
+		user_agent = random.choice(lines).replace("\n", "")
+		f.close()
+		sleep(0.1)
+	return str(user_agent)
 
 proxyType = ''
 
@@ -29,6 +35,7 @@ def checkproxy(txtfile):
 	if args.verbose:
 		print("\n\nCurrent IPs in proxylist: %s\n" % (len(open(txtfile).readlines())))
 
+
 def checker(i):
 	proxy = proxyType + '://' + i
 	proxy_support = urllib.request.ProxyHandler({proxyType: proxy})
@@ -36,7 +43,10 @@ def checker(i):
 	urllib.request.install_opener(opener)
 	global site
 	req = urllib.request.Request(proxyType + '://' + site)
-	req.add_header("User-Agent", useragents)
+	global user_agent
+	if args.random_agent:
+		user_agent = random_user_agent()
+	req.add_header("User-Agent", user_agent)
 	try:
 		global chosenTimeout
 		start_time = time()
@@ -46,32 +56,26 @@ def checker(i):
 		out_file.write(i)
 		if args.verbose:
 			print ("%s works!" % proxy)
-			print('time: ' + str(time_taken) + '\n')
-	except:
+			print('time: ' + str(time_taken))
+			print('user_agent: ' + user_agent +'\n')
+	except Exception as e:
+		print(e)
 		pass
 		if args.verbose:
 			print ("%s does not respond.\n" % proxy)
 
 
-
-
-if __name__ == "__main__":
-
-	global chosenTimeout
-	global txtfile
-	global site
-
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-t", "--timeout", type=int, help="dismiss the proxy after -t seconds", default=20)
-	parser.add_argument("-p", "--proxy", help="check HTTPS or HTTP proxies", default='http')
-	parser.add_argument("-l", "--list", help="path to your list.txt", default='output.txt')
-	parser.add_argument("-s", "--site", help="check with specific website like google.com", default='google.com')
-	parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
-	args = parser.parse_args()
-
-	chosenTimeout = args.timeout
-	txtfile = args.list
-	site = args.site
-	proxyType = args.proxy
-
-	threading.Thread(target=checkproxy, args=(txtfile,)).start()
+# Добавление аргументов в парсер
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--timeout", type=int, help="dismiss the proxy after -t seconds", default=20)
+parser.add_argument("-p", "--proxy", help="check HTTPS or HTTP proxies", default='http')
+parser.add_argument("-l", "--list", help="path to your list.txt", default='output.txt')
+parser.add_argument("-s", "--site", help="check with specific website like google.com", default='https://google.com/')
+parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+parser.add_argument("-r", "--random_agent", help="use a random user agent per proxy", action="store_true")
+args = parser.parse_args() # Получение всех аргументов
+txtfile = args.list # list.txt
+site = args.site # Сайт указанный пользователем (default="https://google.com/")
+proxyType = args.proxy # Какие прокси парсить (default="http")
+chosenTimeout = args.timeout
+threading.Thread(target=checkproxy, args=(txtfile,)).start() # старт
